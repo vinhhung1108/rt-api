8d6c650f6e4a4d1e1d653351953b61ac7ab5a151
vinhhung1108 <31027152+vinhhung1108@users.noreply.github.com>
2022-10-02 14:02:06 +0700

    province

--- src/province/province.controller.ts
+++ src/province/province.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProvinceService } from './province.service';
 import { CreateProvinceDto } from './dto/create-province.dto';
 import { UpdateProvinceDto } from './dto/update-province.dto';
@@ -23,7 +31,10 @@ export class ProvinceController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProvinceDto: UpdateProvinceDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateProvinceDto: UpdateProvinceDto,
+  ) {
     return this.provinceService.update(+id, updateProvinceDto);
   }
 
